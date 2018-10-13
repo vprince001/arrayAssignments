2313ec5a446df7046f78abf1547ae982ff2838bc
Prince Verma <16836599+vprince001@users.noreply.github.com>
2018-10-13 14:19:55 +0530

    Check Ascending Order added
    Test file for question number 17 "Check ascending order in array" added.

--- library/array_lib_16_to_27.js
+++ library/array_lib_16_to_27.js
@@ -1,3 +1,5 @@
+const findLowest = require("./array_lib_1_to_15").findLowest;
+
 const findIndex = function(inputArray,number){
   for(let index in inputArray){
     if(inputArray[index] == number){
@@ -7,4 +9,14 @@ const findIndex = function(inputArray,number){
   return "no match found"
 }
 
+const checkAscendingOrder = function(inputArray){
+  for(let index = 0; index < inputArray.length; index++){
+    if(inputArray[index]>inputArray[index+1]){
+      return false;
+    }
+  }
+  return true;
+}
+
 exports.findIndex = findIndex;
+exports.checkAscendingOrder = checkAscendingOrder;
