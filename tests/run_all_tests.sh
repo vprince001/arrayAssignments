9446766b3a90753f30aadfe3717cf0f8b1c4f613
Prince Verma <16836599+vprince001@users.noreply.github.com>
2018-10-19 23:37:06 +0530

    Refactor run_all_tests.sh "exits on failing"

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -1,86 +1,47 @@
-#!/bin/bash
-echo "@---General Array Tests---@"
+#! /bin/bash
+generalArrayDir="./general_array_tests/"
+generalArrayFiles=$(ls $generalArrayDir)
 
-node ./general_array_tests/test_extract_alternate_elements.js 
-echo "test_extract_alternate_elements passed"
+echo  +===============================+
+echo "|TESTING GENERAL ARRAY FUNCTIONS|"
+echo  +===============================+
 
-node ./general_array_tests/test_map_lengths.js
-echo "test_map_lengths passed"
+for fileName in $generalArrayFiles
+do
+  node $generalArrayDir/$fileName
+  
+  if [ $? != 0 ]
+  then
+    echo $fileName "failed"
+    exit
+  fi
 
-node ./general_array_tests/test_reverse_array.js
-echo "test_reverse_array passed"
+  echo $fileName passed
+  echo ------------------------------
+done
 
-node ./general_array_tests/test_check_ascending_order.js
-echo "test_check_ascending_order passed"
-
-node ./general_array_tests/test_check_descending_order.js
-echo "test_check_descending_order passed"
-
-node ./general_array_tests/test_extract_all_chars.js
-echo "test_extract_all_chars passed"
-
-node ./general_array_tests/test_subtract_set.js
-echo "test_extract_difference passed"
-
-node ./general_array_tests/test_find_index.js
-echo "test_find_index passed"
-
-node ./general_array_tests/test_extract_intersections.js
-echo "test_extract_intersections passed"
-
-node ./general_array_tests/test_is_array_subset.js
-echo "test_is_array_subset passed"
-
-node ./general_array_tests/test_partition_array.js
-echo "test_partition_array passed"
-
-node ./general_array_tests/test_remove_duplicates.js
-echo "test_remove_duplicates passed"
-
-node ./general_array_tests/test_rotate_array.js
-echo "test_rotate_array passed"
-
-node ./general_array_tests/test_union_arrays.js
-echo "test_union_arrays passed"
-
-node ./general_array_tests/test_zip_two_arrays.js
-echo "test_zip_two_arrays passed"
+numberArrayDir="./number_array_tests"
+numberArrayFiles=$(ls $numberArrayDir)
 
 echo ""
-echo "@---Number Array Tests---@"
+echo  +==============================+
+echo "|TESTING NUMBER ARRAY FUNCTIONS|"
+echo  +==============================+
 
-node ./number_array_tests/test_calc_average.js
-echo "test_calc_average passed"
-
-node ./number_array_tests/test_count_evens.js
-echo "test_count_evens passed"
-
-node ./number_array_tests/test_count_numbers_above.js
-echo "test_count_numbers_above passed"
-
-node ./number_array_tests/test_count_numbers_below.js
-echo "test_count_numbers_below passed"
-
-node ./number_array_tests/test_count_odds.js
-echo "test_count_odds passed"
-
-node ./number_array_tests/test_extract_even_elements.js
-echo "test_extract_even_elements passed"
-
-node ./number_array_tests/test_extract_odd_elements.js
-echo "test_extract_odd_elements passed"
-
-node ./number_array_tests/test_find_greatest_element.js
-echo "test_find_greatest_elements passed"
-
-node ./number_array_tests/test_find_lowest_element.js
-echo "test_lowest_elements passed"
-
-node ./number_array_tests/test_reverse_fibo.js
-echo "test_reverse_fibo passed"
-
-node ./number_array_tests/test_sum_all_elements.js
-echo "test_sum_all_elements passed"
+for fileName in $numberArrayFiles
+do
+  node $numberArrayDir/$fileName
+  
+  if [ $? != 0 ]
+  then
+    echo $fileName failed
+    exit
+  fi
+  echo $fileName passed
+  echo ------------------------------
+done
 
 echo ""
-echo "@---ALL TEST PASSED---@"
+echo  +===============+
+echo "|ALL TEST PASSED|"
+echo  +===============+
